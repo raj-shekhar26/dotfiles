9f3d14ce44c2f2c3c492eaf3029c0a5b025d09c9
raj-shekhar26 <mj2929@tutamail.com>
2020-05-22 08:38:03 +0530

    updating bspwm

--- bspwm/.config/fish/config.fish
+++ bspwm/.config/fish/config.fish
@@ -14,14 +14,15 @@ function fish_prompt
     set_color normal
 end
 
-alias ls "ls -a"
+alias ls='ls -a --color=auto'
 alias c "clear"
 alias search "pacman -Ss"
 alias pkgs "pacman -Qe"
 alias install "sudo pacman -S"
 alias update "sudo pacman -Syu"
 alias rmv "sudo pacman -Rns"
+alias remove "sudo pacman -Rns"
 alias orphan "pacman -Qdtq"
 alias cache "sudo pacman -Sc"
 
-
+~/.config/pfetch.sh
